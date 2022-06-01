8d0fdfa537aa3392fa19538a41eb37bd7b39db8a
a.voitishin <a.voitishin@gmail.com>
2022-06-01 10:29:06 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// const Person = require('./Person');
+import { Person } from "./person.js";
+
+const person = new Person('John Doe', 30);
+person.greeting();
---
+++ person.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        console.log(`Hi, my name is ${this.name} and I am ${this.age}`);
+    }
+}
+
+// module.exports = Person;
+export {Person};
